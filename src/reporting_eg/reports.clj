a39a2ad66a5312975fc3e6120a5c3d35fcab7cf1
Ryan Case <rmcase@gmail.com>
2015-04-10 00:47:41 -0600

    Created paths to generate reports and code to invoke them

---
+++ src/reporting_eg/reports.clj
@@ -0,0 +1,35 @@
+(ns reporting_eg.reports
+  (:require [clj-pdf.core :refer [pdf template]]
+            [reporting_eg.models.db :as db]))
+
+(def employee-template
+  (template [$name $occupation $place $country]))
+
+(def employee-template-paragraph
+  (template
+    [:paragraph
+     [:heading {:style {:size 15}} $name]
+     [:chunk {:style :bold} "Occupation: "] $occupation "\n"
+     [:chunk {:style :bold} "Location: "] $place "\n"
+     [:chunk {:style :bold} "Country: "] $country "\n"
+     [:spacer]]))
+
+(defn table-report [out]
+  (pdf
+    [{:header "Employee List"}
+    (into [:table
+           {:border false
+            :cell-border false
+            :header [{:color [0 150 150]} "Name" "Occupation" "Place" "Country"]}]
+          (employee-template (db/read-employees)))]
+    out))
+
+(defn list-report [out]
+  (pdf
+    [{}
+     [:heading {:size 10} "Employees"]
+     [:line]
+     [:spacer]
+     (employee-template-paragraph (db/read-employees))]
+    out))
+
--- src/reporting_eg/routes/home.clj
+++ src/reporting_eg/routes/home.clj
@@ -1,9 +1,40 @@
 (ns reporting-eg.routes.home
-  (:require [compojure.core :refer :all]
-            [reporting-eg.views.layout :as layout]))
+  (:require [compojure.core :refer [defroutes GET]]
+            [ring.util.response :as response]
+            [hiccup.element :refer [link-to]]
+            [reporting-eg.views.layout :as layout]
+            [reporting_eg.reports :as reports]))
 
 (defn home []
-  (layout/common [:h1 "Hello World!"]))
+  (layout/common
+    [:h1 "Select Report:"]
+    [:ul
+     [:li (link-to "/list" "List Report")]
+     [:li (link-to "/table" "Table Report")]]))
+
+(defn write-response [report-bytes]
+  (with-open [in (java.io.ByteArrayInputStream. report-bytes)]
+    (-> (response/response in)
+        (response/header "Content-Disposition" "filename=report.pdf")
+        (response/header "Content-Length" (count report-bytes))
+        (response/content-type "application/pdf"))))
+
+(defn generate-report [report-type]
+  (try
+    (let [out (new java.io.ByteArrayOutputStream)]
+      (condp = (keyword report-type)
+        :table (reports/table-report out)
+        :list  (reports/list-report out))
+      (write-response (.toByteArray out)))
+
+    (catch Exception ex
+      {:status 500
+       :headers {"Content-Type" "text/html"}
+       :body (layout/common
+               [:h2 "An error has occured while generating the report"]
+               [:p (.getMessage ex)])})))
+
 
 (defroutes home-routes
-  (GET "/" [] (home)))
+  (GET "/" [] (home))
+  (GET "/:report-type" [report-type] (generate-report report-type)))
